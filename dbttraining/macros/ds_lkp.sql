e964c8f807dfcb617375dd6dc12cc12058e6b20b
Siddardh Namburi <sid@eoncollective.com>
2021-01-29 10:30:18 -0800

    changes

--- dbttraining/macros/ds_lkp.sql
+++ dbttraining/macros/ds_lkp.sql
@@ -1,22 +1,30 @@
-{% macro generate_lkp_query(src_tbl,lkp_tbl,jointype,joinoperator,srcfield,tgtfield,field_names) %}
+{% macro generate_lkp_query(src_tbl,lkp_tbl,jointype,joinoperator,srcfield,tgtfield,src_field_names,lkp_field_names) %}
+
+--{% set jointype=  join_type | replace("[", "") | replace("]", "") | replace("'","")  %}
+{% set joinoperator=  joinoperator | replace("[", "") | replace("]", "") | replace("'","") %}
+{% set srcfield=  srcfield | replace("[", "") | replace("]", "") | replace("'","") %}
+{% set tgtfield=  tgtfield | replace("[", "") | replace("]", "") | replace("'","") %}
+{% set src_field_names=  src_field_names | replace("[", "") | replace("]", "") | replace("'","") %}
+{% set lkp_field_names=  lkp_field_names | replace("[", "") | replace("]", "") | replace("'","") %}
+
     {%- if jointype == 'I' -%}
-   (SELECT {{field_names}} from ({{src_tbl}}) as src_tbl INNER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}})
+   (SELECT {{src_field_names}} , {{lkp_field_names}} from ({{src_tbl}}) as src_tbl INNER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}})
         
 
     {%- elif jointype == 'L' -%}
-   (SELECT {{field_names}} from ({{src_tbl}}) as src_tbl LEFT OUTER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}} )
+   (SELECT {{src_field_names}} , {{lkp_field_names}}  from ({{src_tbl}}) as src_tbl LEFT OUTER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}} )
         
 
     {%- elif jointype == 'R' -%}
-   (SELECT {{field_names}} from ({{src_tbl}}) as src_tbl RIGHT OUTER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}} )
+   (SELECT {{src_field_names}} , {{lkp_field_names}}  from ({{src_tbl}}) as src_tbl RIGHT OUTER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}} )
         
 
     {%- elif jointype == 'F' -%}
-   (SELECT {{field_names}} from ({{src_tbl}}) as src_tbl FULL OUTER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}} )
+   (SELECT {{src_field_names}} , {{lkp_field_names}}  from ({{src_tbl}}) as src_tbl FULL OUTER JOIN ({{lkp_tbl}}) as lkp_tbl on {{srcfield}}  {{joinoperator}} {{tgtfield}} )
 
 
     {%- else -%}
-    (SELECT {{field_names}} from ({{src_tbl}}) as src_tbl , ({{lkp_tbl}}) as lkp_tbl where {{srcfield}}  {{joinoperator}} {{tgtfield}})
+    (SELECT {{src_field_names}} , {{lkp_field_names}}  from ({{src_tbl}}) as src_tbl , ({{lkp_tbl}}) as lkp_tbl where {{srcfield}}  {{joinoperator}} {{tgtfield}})
     {%- endif -%}
 
 {%- endmacro %}
--- dbttraining/models/dstodbt/lkp_busi_dt.sql
+++ dbttraining/models/dstodbt/lkp_busi_dt.sql
@@ -1,6 +1,62 @@
-{{ config(materialized='view') }}
 
- {{ generate_lkp_query( ref('seq_hme_detail')  , ref('odbc_time_day_dim') , 'I', '=', 'BUSINESS_DATE',  'BUSIDAYDT',  
-    dbtvault.stage(include_source_columns=var('include_source_columns', none),source_model=var('source_model', none),hashed_columns=var('hashed_columns', none) ,derived_columns=var('derived_columns', none)
- )     
- )  }}
+{%- set yaml_metadata -%}
+--source_model: "raw_source"
+src_column_list:
+    is_hashdiff: true
+    columns:
+        - 'STORE_ID'
+        - 'DW_STOREID'
+        - 'BUSINESS_DATE'
+        - 'OPEN_TIME'
+        - 'CLOSE_TIME' 
+        - 'TRANSACTION_DATE'
+        - 'TRANS_DATE_TXT'
+        - 'TRANSACTION_TIME' 
+        - 'GREET_DELAY'
+        - 'MENU_BOARD_DURATION'
+        - 'QUEUE_DURATION'
+        - 'CASHIER_WINDOW_DURATION'
+        - 'BOOTH_QUEUE_DURATION'
+        - 'SERVICE_WINDOW_DURATION'
+        - 'TOTAL_BOOTH_DURATION'
+        - 'WAITING_AREA_DURATION'
+        - 'TRANSACTION_TIME_LKP'
+        - 'TOTAL_SPEED_OF_SERVICE_DURATION'
+        - 'LANE_NUMBER'
+        - 'TBC_LANE'
+lkp_column_list:
+    columns:
+        - 'DW_DAY'
+source_tbl_join_field:
+    columns:
+        - 'BUSINESS_DATE'
+lookup_tbl_join_field:
+    columns:
+        - 'BUSIDAYDT'
+{% endset %}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{% set join_operator = '=' %}
+{% set join_type = 'I' %}
+{% set source_tbl_join_field = 'BUSINESS_DATE' %}
+{% set lookup_tbl_join_field = 'BUSIDAYDT' %}
+
+{% set source_columns = metadata_dict['src_column_list'] %}
+
+
+{% set lkp_columns = metadata_dict['lkp_column_list'] %}
+
+
+--{% set ranked_columns = metadata_dict['ranked_columns'] %}
+
+ {{ config(materialized='view') }}
+
+ {{ generate_lkp_query( ref('seq_hme_detail')  , ref('odbc_time_day_dim') , 
+ 'I', 
+ join_operator, 
+ source_tbl_join_field, 
+ lookup_tbl_join_field ,    
+ source_columns['columns'], 
+ lkp_columns['columns']  ~ ' as DW_BUSI_DAY')    
+ }}
