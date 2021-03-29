ae533bee5a8af18fce6fe2725cfb25bfa703d170
Sid <sid@eoncollective.com>
2021-03-29 09:40:43 -0400

    changes pxjoin

--- models/fromrobert/PxJoin_new.sql
+++ models/fromrobert/PxJoin_new.sql
@@ -6,10 +6,10 @@
 source_model: "jn_srcs_lnk_src_addr"
 lkp_models: ["jn_srcs_lnk_src_addr", "jn_srcs_Lnk_src_nas","jn_srcs_Lnk_src_appl","jn_srcs_Lnk_pvt_seg"]
 join_type: "left outer join"
-join_keys: ["APP_PKG_ID","USAA_PARTY_PD_ID"]
+join_keys: ["APP_PKG_ID","PARTY_PD_ID"]
 derived_columns:
     APP_PKG_ID: "jn_srcs_lnk_src_addr.APP_PKG_ID"
-    USAA_PARTY_PD_ID: "jn_srcs_lnk_src_addr.USAA_PARTY_PD_ID"
+    PARTY_PD_ID: "jn_srcs_lnk_src_addr.PARTY_PD_ID"
     DEP_CUST_RSK_SCR_SVC_FAIL_CD: "jn_srcs_lnk_src_addr.DEP_CUST_RSK_SCR_SVC_FAIL_CD"
     DEP_UW_MBR_NR_RESC_CD: "jn_srcs_lnk_src_addr.DEP_UW_MBR_NR_RESC_CD"
     MBR_RLTN_VAR_FAIL_CD: "jn_srcs_lnk_src_addr.MBR_RLTN_VAR_FAIL_CD"
