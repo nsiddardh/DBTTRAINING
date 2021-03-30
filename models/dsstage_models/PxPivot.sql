cc1b9a3736c7d77d9d02f36e0687524064a7cfc1
Sid <sid@eoncollective.com>
2021-03-30 10:40:24 -0400

    pxpivot changes

--- models/dsstage_models/PxPivot.sql
+++ models/dsstage_models/PxPivot.sql
@@ -6,15 +6,19 @@ source_model: "monthly_sales"
 pivot_type: "Vertical"
 group_column: "empid"
 agg_column: "amount"
-pivot_column: ["month","month"]
+pivot_column: ["month","year"]
 array_index: 4
 
 derived_columns:
     emp_id: 'empid'
+    month: 'month'
     Month_1: 'Month_1'
     Month_2: 'Month_2'
     Month_3: 'Month_3'
-
+    year: 'year'
+    Year_1: 'Year_1'
+    Year_2: 'Year_2'
+    Year_3: 'Year_3'
 
 {% endset %}
 
