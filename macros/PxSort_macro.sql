501cd14cf84ef0c1ea7c0ca50bdd02c24d48b707
Siddardh Namburi <sid@eoncollective.com>
2021-07-04 05:00:11 -0400

    pxsort changes

--- macros/PxSort_macro.sql
+++ macros/PxSort_macro.sql
@@ -3,7 +3,9 @@
 
 {% set sort_columns = metadata_dict['sort_columns'] %}
 {% set source_model = metadata_dict['source_model'] %}
+{% set derived_columns = metadata_dict['derived_columns'] %}
 
-select * from {{ source_model }} order by {{ sort_columns }}
+select {{ create_alias(  source_model=source_model, derived_columns=derived_columns) }} 
+from {{ source_model }} order by {{ sort_columns }}
 
 {% endmacro %}
