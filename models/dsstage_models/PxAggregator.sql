6db62564770999da2e6c1b4f680d342d2412543a
Siddardh Namburi <sid@eoncollective.com>
2021-07-06 08:14:14 -0400

    aggregator chnages

---
+++ models/dsstage_models/PxAggregator.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='view') }}
+
+
+{%- set yaml_metadata -%}
+source_model: "emp"
+group_by_columns: "deptid, empid"
+derived_columns:
+    deptid: "deptid"
+    empid: "empid"
+    max_sal: "max(sal)"
+
+{% endset %}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ PxAggregator_macro(metadata_dict) }}
--- target/compiled/dbttraining/models/dsstage_models/PxAggregator.sql
+++ target/compiled/dbttraining/models/dsstage_models/PxAggregator.sql
@@ -2,21 +2,10 @@
 
 
 
-
-
-
-
-
-with source_data as (
-SELECT  TIMEZONE AS TIMEZONE,
-    TZ_CNT AS TZ_CNT,
-    ERS_CNT AS ERS_CNT,
-    Ttl_ERS AS Ttl_ERS,
-    CURR_TIME AS CURR_TIME,
-    RUN_TIME AS RUN_TIME,
-    sum(TBE_Recvd) AS TBE_Recvd
-FROM i_tbc_hme_timer_detail as XFM_HME
-group by TIMEZONE,TZ_CNT,ERS_CNT,Ttl_ERS,CURR_TIME,RUN_TIME
-)
-
-select * from source_data
+SELECT deptid AS deptid,
+    empid AS empid,
+    max(sal) AS max_sal 
+FROM emp
+GROUP BY 
+deptid, empid
+    
